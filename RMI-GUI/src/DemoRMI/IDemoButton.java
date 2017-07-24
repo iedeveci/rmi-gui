e2fedf3df6854f073ad8dacd8e49f850fbddcca5
mustafagoksever <mustafagoksever@hotmail.com>
2017-07-24 14:30:40 +0300

    Refreshed

--- RMI-GUI/src/DemoRMI/IDemoButton.java
+++
@@ -1,8 +0,0 @@
-package DemoRMI;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IDemoButton extends Remote {
-	public String doaction(String ss) throws RemoteException;
-}
