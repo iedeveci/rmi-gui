8060e1714c148832c03df4e1bff9ea04f20d1f85
İbrahim Ethem Deveci <ideveci14@ku.edu.tr>
2017-07-24 14:24:57 +0300

    Add files via upload

---
+++ RMI-GUI/src/DemoRMI/IDemoButton.java
@@ -0,0 +1,8 @@
+package DemoRMI;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IDemoButton extends Remote {
+	public String doaction(String ss) throws RemoteException;
+}
---
+++ RMI-GUI/src/JavaUnit/EventTest.java
@@ -0,0 +1,17 @@
+package JavaUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Client.DemoClient;
+
+public class EventTest {
+
+	@Test
+	public void test() {
+		int sonuc = DemoClient.multiplication("5", "8");
+		assertEquals(40, sonuc);
+	}
+
+}
