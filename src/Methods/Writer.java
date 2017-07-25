2b6e93cf9cbf1aa0039b125f4328fdae73558162
mustafagoksever <mustafagoksever@hotmail.com>
2017-07-25 09:56:59 +0300

    updated

---
+++ src/Methods/Writer.java
@@ -0,0 +1,31 @@
+package Methods;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Writer {
+	private static final String FILENAME = "WriterMessages.txt";
+
+	public static Writer writeToFile(Event FileEvent) {
+		try {
+
+			String content = FileEvent.serialize().toString();
+			File file = new File(FILENAME);
+			FileWriter fw = new FileWriter(file, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content + " ");
+			if ("WriterMessages.txt" != null) {
+				bw.newLine();
+			}
+			bw.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+		return null;
+	}
+
+}
