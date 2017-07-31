2f3224c9536ac544baf47d82db6ca4d800f52d23
mustafagoksever <mustafagoksever@hotmail.com>
2017-07-31 11:56:45 +0300

    added test runner

--- src/JavaUnit/Runner.java
+++ src/JavaUnit/Runner.java
@@ -5,7 +5,7 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-public class TestRunner {
+public class Runner {
    public static void main(String[] args) {
       Result result = JUnitCore.runClasses(AllTests.class);
 		
--- src/Runner.java
+++ src/Runner.java
@@ -3,7 +3,7 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-public class TestRunner {
+public class Runner {
    public static void main(String[] args) {
       Result result = JUnitCore.runClasses(AllTests.class);
 		
